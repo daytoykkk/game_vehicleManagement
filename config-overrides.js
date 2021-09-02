f249b27b3a58433d3f389aef1ab3a6f03417809b
daytoykkk <59024183+daytoykkk@users.noreply.github.com>
2021-09-02 21:34:12 +0800

    引入antd

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+    // 针对antd实现按需打包：根据import来打包
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,   //自动打包相关的样式
+    }),
+    // 自定义主题
+    addLessLoader({
+        lessOptions:{
+            javascriptEnabled: true,
+            modifyVars: { 
+                '@primary-color': '#648cb9',
+                '@border-radius-base': '4px',
+            },
+        }
+    }),
+);
--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// 应用的根组件
+import React, { Component } from 'react'
+import { Button, message } from 'antd';
+import './App.less';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+    handleClick = () => {
+        message.success("哈哈哈哈哈")
+    }
+
+    render() {
+        return (
+            <div>
+               <Button type="primary" onClick={this.handleClick}>Button</Button>
+            </div>
+        )
+    }
 }
-
-export default App;
