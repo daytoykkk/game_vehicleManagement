88c780e796f3560307f3edee8060933741aa9252
daytoykkk <59024183+daytoykkk@users.noreply.github.com>
2021-09-06 16:08:15 +0800

    完成登陆界面

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,13 +1,52 @@
 import React, { Component } from 'react'
 import './index.less';
-import { Button } from 'antd';
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 export default class Login extends Component {
+
+    handleLogin = (values) => {
+        console.log('数据: ', values);
+      };
+
     render() {
         return (
-            <div>
-                我是登录
-                <Button type="primary">hhh</Button>
+            <div className="login">
+                <div className="header">
+                    车辆管理平台
+                </div>
+                <div className="content">
+                    <h2>管理员登陆</h2>
+                    <Form
+                        name="login_form"
+                        className="login-form"
+                        initialValues={{ remember: false }}
+                        onFinish={this.handleLogin}
+                    >
+                        <Form.Item
+                            name="username"
+                            rules={[{ required: true, message: '请输入用户名！' }]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[{ required: true, message: '请输入密码！' }]}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                type="password"
+                                placeholder="密码"
+                            />
+                        </Form.Item>
+                       
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                登陆
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
             </div>
         )
     }
