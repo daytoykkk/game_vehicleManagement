cf6cb1c7668ef97c4923f456183b0e2e8cf8a2c7
Kelly Hong <411487840@qq.com>
2021-09-17 23:11:08 +0800

    feat:加了websocket

--- src/pages/guard/index.jsx
+++ src/pages/guard/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import './index.less';
-import { Layout } from 'antd';
+import { Layout, message } from 'antd';
 import LeftNav from '../../components/left-nav';
 import HeaderTop from '../../components/header';
 import GuardStall from '../guardStall';
@@ -13,9 +13,43 @@ const { Sider, Content } = Layout;
 export default class Guard extends Component {
 
     state = {
-        collapsed: false
+        collapsed: false,
+        
     }
 
+    ws = null
+    token = sessionStorage.getItem("token") ? sessionStorage.getItem("token") : ''
+    url = "ws://z3773e6368.qicp.vip/socket/"
+    connected = false
+    connectRetryCount = 0
+    connectCount = 0
+    
+    UNSAFE_componentWillMount = () => {
+        this.handleInitWs()
+    }
+
+    // 连接ws
+    handleInitWs = () => {
+        this.ws = new WebSocket(this.url + this.token);
+        this.ws.onopen = () => {
+          console.log("连接服务端成功了");
+          this.connected = true;
+          this.connectRetryCount = 0;
+        };
+        this.ws.onclose = () => {
+          message.error("连接服务端失败");
+          this.connected = false;
+          this.connectRetryCount++;
+          setTimeout(() => {
+            this.handleInitWs();
+          }, 500 * this.connectRetryCount);
+        };
+        this.ws.onmessage = (res) => {
+            message.success(res.data)
+        };
+      };
+
+    // 菜单伸缩
     toggle = () => {
         this.setState({
             collapsed: !this.state.collapsed
@@ -35,7 +69,7 @@ export default class Guard extends Component {
                             <Switch>
                                 <Route path='/guard/stall' component={GuardStall} />
                                 <Route path='/guard/log' component={GuardLog} />
-                                <Redirect from="/guard" to="/guard/log" />
+                                <Redirect from="/guard" to="/guard/stall" />
                             </Switch>
                         </Content>
                     </Layout>
